52fa18246e12bb6c83262276dc5898315b70a060
samuriverosb <samuel.riverosb@gmail.com>
2023-01-03 13:32:38 -0300

    Assignment Portal Submitted

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+const date = new Date();
+const year = date.getFullYear();
+document.querySelector('#year').textContent = year;
+
+const lastModified = new Date(document.lastModified)
+const month = lastModified.getMonth();
+const day = lastModified.getDay();
+const hour = lastModified.getHours();
+const minutes = lastModified.getMinutes();
+const seconds = lastModified.getSeconds();
+const fullTime = `${month + 1}/${day + 1}/${year} ${hour}:${minutes}:${seconds}`
+document.querySelector('#date').textContent = fullTime;
