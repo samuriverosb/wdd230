be4ed0a24d728c377cafe46d86c3bea4ad5e90e6
samuriverosb <samuel.riverosb@gmail.com>
2023-01-09 15:46:22 -0300

    Design Principles Completed

--- scripts/script.js
+++ scripts/script.js
@@ -8,5 +8,5 @@ const day = lastModified.getDay();
 const hour = lastModified.getHours();
 const minutes = lastModified.getMinutes();
 const seconds = lastModified.getSeconds();
-const fullTime = `${month + 1}/${day + 1}/${year} ${hour}:${minutes}:${seconds}`
+const fullTime = `${month + 1}/${day + 1}/${year} ${hour}:${minutes < 10 ? "0" + minutes : minutes}:${seconds < 10 ? "0" + seconds : seconds}`
 document.querySelector('#date').textContent = fullTime;
