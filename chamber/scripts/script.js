28476ae451bd459e74a057b17f63d5d901a47386
samuriverosb <samuel.riverosb@gmail.com>
2023-01-24 19:48:51 -0300

    Added VMChamber Home Page

---
+++ chamber/scripts/script.js
@@ -0,0 +1,20 @@
+const year = document.querySelector("#year");
+year.textContent = new Date().getFullYear();
+
+const date = document.querySelector("#date");
+date.textContent = new Date().toLocaleDateString("en-UK", {
+  weekday: "long",
+  day: "numeric",
+  year: "numeric",
+  month: "long"
+});
+
+const lastModified = document.querySelector("#modification");
+lastModified.textContent = new Date(document.lastModified).toLocaleString();
+
+const toggleMenu = () => {
+  document.querySelector("#hidden").classList.toggle("show");
+}
+
+const hamburger = document.querySelector(".hamburger")
+hamburger.onclick = toggleMenu;
