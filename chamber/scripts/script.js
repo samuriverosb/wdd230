0d814a63c3068b029f617fbc99e0f4a88514d938
samuriverosb <samuel.riverosb@gmail.com>
2023-01-30 23:15:02 -0300

    Added content to Chamber Page

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -1,8 +1,9 @@
+const getDate = new Date();
 const year = document.querySelector("#year");
-year.textContent = new Date().getFullYear();
+year.textContent = getDate.getFullYear();
 
 const date = document.querySelector("#date");
-date.textContent = new Date().toLocaleDateString("en-UK", {
+date.textContent = getDate.toLocaleDateString("en-UK", {
   weekday: "long",
   day: "numeric",
   year: "numeric",
@@ -18,3 +19,26 @@ const toggleMenu = () => {
 
 const hamburger = document.querySelector(".hamburger")
 hamburger.onclick = toggleMenu;
+
+const eventDate = document.querySelector(".eventHidden")
+const day = getDate.getDay();
+if (day == 1 || day == 2) {
+  eventDate.classList.toggle("showEvent");
+}
+
+const cityName = document.querySelector("#city-name");
+const degrees = document.querySelector("#degrees");
+const weatherIcon = document.querySelector("#weather-icon");
+const description = document.querySelector("#description");
+
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?id=3868121&appid=f6d1be44d84a90316be8e41a8096a413&units=metric"
+fetch(API_URL)
+  .then(res => res.json())
+  .then(data => {
+    cityName.textContent = data.name;
+    degrees.textContent = parseInt(data.main.temp);
+    description.textContent = data.weather[0].description;
+    weatherIcon.setAttribute("src", 
+      `http://openweathermap.org/img/w/${data.weather[0].icon}.png`  
+    )
+  })
